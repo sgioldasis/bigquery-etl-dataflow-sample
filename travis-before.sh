4633ea3c1cde32e5a68ec17d9793851123dcb2f3
Savas Gioldasis <gioldasis@pythian.com>
2018-12-26 15:56:55 +0200

    CHG: Protobuf 3.6.0

--- travis-before.sh
+++ travis-before.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-PROTOBUF_VERSION=3.3.0
+PROTOBUF_VERSION=3.6.0
 PROTOC_FILENAME=protoc-${PROTOBUF_VERSION}-linux-x86_64.zip
-
 wget https://github.com/google/protobuf/releases/download/v${PROTOBUF_VERSION}/${PROTOC_FILENAME}
 unzip ${PROTOC_FILENAME}
 bin/protoc --version
