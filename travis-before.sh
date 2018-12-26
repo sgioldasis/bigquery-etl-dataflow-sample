1b2142788e97f641417072863ab3c9f9837294bf
Savas Gioldasis <gioldasis@pythian.com>
2018-12-26 15:41:58 +0200

    CHG: Remove pushd/popd

--- travis-before.sh
+++ travis-before.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 PROTOBUF_VERSION=3.3.0
 PROTOC_FILENAME=protoc-${PROTOBUF_VERSION}-linux-x86_64.zip
-pushd /home/travis
+
 wget https://github.com/google/protobuf/releases/download/v${PROTOBUF_VERSION}/${PROTOC_FILENAME}
 unzip ${PROTOC_FILENAME}
 bin/protoc --version
-popd
